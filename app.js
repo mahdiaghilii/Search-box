7ed9916ddfaf3e9fe97f9cdbf5df75fe5f7d178a
mahdi aghili <mahdi.agheyli@gmail.com>
2025-11-24 21:24:51 +0330

    creat search bar

--- app.js
+++ app.js
@@ -8,11 +8,13 @@ fetch('json/full_store_mock_optimized.json')
     
     let ObjProducts = resp.products.slice(0 ,100)
     ObjProducts.forEach(element => {
-        console.log(element)    
+        // console.log(element)    
         
         let ProductCard = document.createElement('div')
+        ProductCard.classList.add('PRcard')
 
         let Title = document.createElement('h2')
+        Title.classList.add('title')
         Title.innerText = element.title.en
 
         let Photo = document.createElement('img')
@@ -31,8 +33,35 @@ fetch('json/full_store_mock_optimized.json')
         ProductCard.append(Price)
         container.append(ProductCard)
     });
+
+    let H2 = document.querySelectorAll('#container h2').forEach(item =>{
+
+    })
     
 })
 
 // show product in page
 
+//creat a search bar
+
+let MySearchBox = document.getElementById('searchBox')
+
+MySearchBox.addEventListener('input' , function(e){
+    const searchText = e.target.value.trim().toLowerCase();
+    const searchCard = document.querySelectorAll('.PRcard')
+    console.log(searchText)
+    
+
+    searchCard.forEach((item)=>{
+        const cardTitle = item.querySelector('.title').textContent.trim().toLowerCase()
+
+        if(cardTitle.includes(searchText)){
+            item.style.display = 'block' //show products
+        } else{
+            item.style.display = 'none' //hide products
+        }
+    })
+
+})
+
+//creat a search bar
